0ff4420d0483ec0039c19efcca5ecdbcaf6d3503
quandev99 <nguyenvietquan2605@gmail.com>
2024-04-30 23:09:25 +0700

    Example 1 React Flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,77 @@
 import React, { useCallback } from "react";
 import ReactFlow, {
+  addEdge,
   MiniMap,
   Controls,
   Background,
   useNodesState,
   useEdgesState,
-  addEdge,
 } from "reactflow";
 
+import {
+  nodes as initialNodes,
+  edges as initialEdges,
+} from "./initial-elements";
+
 import "reactflow/dist/style.css";
+import "./index.css";
+import "./App.css";
+import CustomNode from "./CustomNode";
 
-const initialNodes = [
-  { id: "1", type:"input",position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  { id: "3", position: { x: 0, y: 200 }, data: { label: "3" } },
-];
-const initialEdges = [
-  {
-    id: "e1-2",
-    source: "1",
-    target: "2",
-    type: "straight",
-    // animated: true,
-    label: "straight",
-  },
-  {
-    id: "e1-2",
-    source: "2",
-    target: "3",
-    label: "nomal",
-  },
-];
+const nodeTypes = {
+  custom: CustomNode,
+};
 
-export default function App() {
+const minimapStyle = {
+  height: 120,
+};
+
+const onInit = (reactFlowInstance) =>
+  console.log("flow loaded:", reactFlowInstance);
+
+const OverviewFlow = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   );
+  // const onConnect = useCallback(
+  //   (connection) =>
+  //     setEdges((eds) => addEdge({ ...connection, animated: true }, eds)),
+  //   [setEdges]
+  // );
+  // we are using a bit of a shortcut here to adjust the edge type
+  // this could also be done with a custom edge for example
+  const edgesWithUpdatedTypes = edges.map((edge) => {
+    if (edge.sourceHandle) {
+      const edgeType = nodes.find((node) => node.type === "custom").data
+        .selects[edge.sourceHandle];
+      edge.type = edgeType;
+    }
+
+    return edge;
+  });
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <ReactFlow
         nodes={nodes}
-        edges={edges}
+        edges={edgesWithUpdatedTypes}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onInit={onInit}
         fitView
+        attributionPosition="top-right"
+        nodeTypes={nodeTypes}
       >
+        <MiniMap style={minimapStyle} zoomable pannable />
         <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
+        <Background color="#aaa" gap={16} />
       </ReactFlow>
     </div>
   );
-}
+};
+
+export default OverviewFlow;
